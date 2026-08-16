aafe0d298bb070dc35009c2c3da696229932dd91
Smit Makwana <smitrmakwana.82@gmail.com>
2026-08-16 11:56:29 +0530

    adding pattern c program

---
+++ C Programs/pattern.c
@@ -0,0 +1,11 @@
+// patern.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("\t\t\t\t*\n");
+	printf("\t\t\t*\t\t*\n");
+	printf("\t\t*\t\t*\t\t*\n");
+	printf("\t*\t\t*\t\t*\t\t*\n");
+	printf("*\t\t*\t\t*\t\t*\t\t*\n");
+}
